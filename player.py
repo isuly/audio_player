b488139d0ae2cdfdf53436889b123b0c25c28739
ifakhretdinova <ifakhretdinova@fix.ru>
2021-02-23 14:30:14 +0300

    add run, pause functions

--- player.py
+++ player.py
@@ -9,31 +9,33 @@ class Player:
         self.stream = None
 
     def play_file(self, fname):
-        # create an audio object
         wf = wave.open(fname, 'rb')
         p = pyaudio.PyAudio()
         chunk = 1024
 
-        # open stream based on the wave object which has been input.
         self.stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                              channels=wf.getnchannels(),
                              rate=wf.getframerate(),
                              output=True)
 
-        # read data (based on the chunk size)
         data = wf.readframes(chunk)
 
-        # play stream (looping from beginning of file to the end)
-        while len(data) > 0:
+        run = True
+
+        while True:
             i, o, e = select.select([sys.stdin], [], [], 0)
             if i:
-                if sys.stdin.readline().strip() == "stop":
+                command = sys.stdin.readline().strip()
+                if command == "run":
+                    run = True
+                elif command == "pause":
+                    run = False
+                elif command == "stop":
                     break
-            # writing to the stream is what *actually* plays the sound.
-            self.stream.write(data)
-            data = wf.readframes(chunk)
+            if run and len(data) > 0:
+                self.stream.write(data)
+                data = wf.readframes(chunk)
 
-            # cleanup stuff.
         self.stream.close()
         p.terminate()
 
